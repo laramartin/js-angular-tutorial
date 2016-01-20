6ee2be79510cf220f47d988fcd13c45ef043ba97
Lara Martin <larikimc@gmail.com>
2016-01-20 12:52:40 +0100

    controller example

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
-# angularJS is called by angular
-# "store" is the app name
-# [] are the dependencies
-var app = angular.module("store", []);
+(function(){
+	 // angularJS is called by angular
+	 // "store" is the app name
+	 // [] are the dependencies
+	var app = angular.module("store", []);
+
+		// Controllers are where we define the app's behavior
+		// by defining functions and values
+		// 2nd argument is a function that is called when 
+		// StoreController is called
+	app.controller("StoreController", function(){
+		this.product = gem;
+	});
+
+	var gem = {
+		name: "Dodecahedron", 
+		price: 2.95,
+		description: "Some gems have hidden qualities beyond their luster, beyond their shine... Dodeca is one of those gems.",
+	}
+
+})();
+
+
+	
+
