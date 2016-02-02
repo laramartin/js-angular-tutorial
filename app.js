4cbc720248e12d31aa44b5ef7b4483e70f5b24b3
Lara Martin <larikimc@gmail.com>
2016-02-02 19:21:15 +0100

    panel controller moved to app.directive
    also moved the <section ng-controller="PanelController as panel">  from
    index.html to a new html called product-panels.html

--- app.js
+++ app.js
@@ -12,17 +12,6 @@
 		this.products = gems;
 	});
 
-	app.controller("PanelController", function(){
-		this.tab = 1;
-
-		this.selectTab = function(setTab){
-			this.tab = setTab;
-		};
-
-		this.isSelected = function(checkTab){
-			return this.tab === checkTab;
-		};
-	});
 
 	app.controller("ReviewController", function(){
 		this.review = {};
@@ -43,6 +32,25 @@
 			restrict: "E",
 			/*url of a template*/
 			templateUrl: "product-title.html"
+		};
+	});
+
+	app.directive("productPanels", function(){
+		return {
+			restrict: "E",
+			templateUrl: "product-panels.html",
+			controller: function(){
+				this.tab = 1;
+
+				this.selectTab = function(setTab){
+					this.tab = setTab;
+				};
+
+				this.isSelected = function(checkTab){
+					return this.tab === checkTab;
+				};
+			},
+			controllerAs: "panels"
 		};
 	});
 
